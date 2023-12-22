22601a2e12ef57a03095c8f5a0407bdcf4895da6
Benjamin <benjamin.mico@pyypl.com>
2023-12-22 11:29:38 +0100

    feat(game): add cards assets

---
+++ assets/cards/index.ts
@@ -0,0 +1,125 @@
+import AceClover from 'assets/cards/AceClover.png';
+import AceHeart from 'assets/cards/AceHeart.png';
+import AcePike from 'assets/cards/AcePike.png';
+import AceTile from 'assets/cards/AceTile.png';
+import Artemis from 'assets/cards/Artemis.png';
+import Clover from 'assets/cards/Clover.png';
+import EightClover from 'assets/cards/EightClover.png';
+import EightHeart from 'assets/cards/EightHeart.png';
+import EightPike from 'assets/cards/EightPike.png';
+import EightTile from 'assets/cards/EightTile.png';
+import FiveClover from 'assets/cards/FiveClover.png';
+import FiveHeart from 'assets/cards/FiveHeart.png';
+import FivePike from 'assets/cards/FivePike.png';
+import FiveTile from 'assets/cards/FiveTile.png';
+import FourClover from 'assets/cards/FourClover.png';
+import FourHeart from 'assets/cards/FourHeart.png';
+import FourPike from 'assets/cards/FourPike.png';
+import FourTile from 'assets/cards/FourTile.png';
+import Hades from 'assets/cards/Hades.png';
+import Heart from 'assets/cards/Heart.png';
+import HiddedCard from 'assets/cards/HiddedCard.png';
+import Hypnos from 'assets/cards/Hypnos.png';
+import JackClover from 'assets/cards/JackClover.png';
+import JackHeart from 'assets/cards/JackHeart.png';
+import JackPike from 'assets/cards/JackPike.png';
+import JackTile from 'assets/cards/JackTile.png';
+import JokerOuranos from 'assets/cards/JokerOuranos.png';
+import KingClover from 'assets/cards/KingClover.png';
+import KingHeart from 'assets/cards/KingHeart.png';
+import KingPike from 'assets/cards/KingPike.png';
+import KingTile from 'assets/cards/KingTile.png';
+import NineClover from 'assets/cards/NineClover.png';
+import NineHeart from 'assets/cards/NineHeart.png';
+import NinePike from 'assets/cards/NinePike.png';
+import NineTile from 'assets/cards/NineTile.png';
+import Pike from 'assets/cards/Pike.png';
+import QueenClover from 'assets/cards/QueenClover.png';
+import QueenHeart from 'assets/cards/QueenHeart.png';
+import QueenPike from 'assets/cards/QueenPike.png';
+import QueenTile from 'assets/cards/QueenTile.png';
+import SevenClover from 'assets/cards/SevenClover.png';
+import SevenHeart from 'assets/cards/SevenHeart.png';
+import SevenPike from 'assets/cards/SevenPike.png';
+import SevenTile from 'assets/cards/SevenTile.png';
+import SixClover from 'assets/cards/SixClover.png';
+import SixHeart from 'assets/cards/SixHeart.png';
+import SixPike from 'assets/cards/SixPike.png';
+import SixTile from 'assets/cards/SixTile.png';
+import TenClover from 'assets/cards/TenClover.png';
+import TenHeart from 'assets/cards/TenHeart.png';
+import TenPike from 'assets/cards/TenPike.png';
+import TenTile from 'assets/cards/TenTile.png';
+import ThreeClover from 'assets/cards/ThreeClover.png';
+import ThreeHeart from 'assets/cards/ThreeHeart.png';
+import ThreePike from 'assets/cards/ThreePike.png';
+import ThreeTile from 'assets/cards/ThreeTile.png';
+import Tile from 'assets/cards/Tile.png';
+import TwoClover from 'assets/cards/TwoClover.png';
+import TwoHeart from 'assets/cards/TwoHeart.png';
+import TwoPike from 'assets/cards/TwoPike.png';
+import TwoTile from 'assets/cards/TwoTile.png';
+
+export {
+  Tile,
+  Pike,
+  Clover,
+  Heart,
+  JokerOuranos,
+  Artemis,
+  Hades,
+  Hypnos,
+  HiddedCard,
+  TwoClover,
+  ThreeClover,
+  FourClover,
+  FiveClover,
+  SixClover,
+  SevenClover,
+  EightClover,
+  NineClover,
+  TenClover,
+  JackClover,
+  QueenClover,
+  KingClover,
+  AceClover,
+  TwoTile,
+  ThreeTile,
+  FourTile,
+  FiveTile,
+  SixTile,
+  SevenTile,
+  EightTile,
+  NineTile,
+  TenTile,
+  JackTile,
+  QueenTile,
+  KingTile,
+  AceTile,
+  TwoHeart,
+  ThreeHeart,
+  FourHeart,
+  FiveHeart,
+  SixHeart,
+  SevenHeart,
+  EightHeart,
+  NineHeart,
+  TenHeart,
+  JackHeart,
+  QueenHeart,
+  KingHeart,
+  AceHeart,
+  TwoPike,
+  ThreePike,
+  FourPike,
+  FivePike,
+  SixPike,
+  SevenPike,
+  EightPike,
+  NinePike,
+  TenPike,
+  JackPike,
+  QueenPike,
+  KingPike,
+  AcePike,
+};
