a2dfeb70e0b1136159c62ed1a88bfea348d0f532
Benjamin <benjamin.mico@pyypl.com>
2023-12-30 16:35:51 +0100

    feat(game): add cards assets

--- assets/cards/index.ts
+++ assets/cards/index.ts
@@ -1,125 +1,125 @@
-import AceClover from 'assets/cards/AceClover.png';
-import AceHeart from 'assets/cards/AceHeart.png';
-import AcePike from 'assets/cards/AcePike.png';
-import AceTile from 'assets/cards/AceTile.png';
-import Artemis from 'assets/cards/Artemis.png';
-import Clover from 'assets/cards/Clover.png';
-import EightClover from 'assets/cards/EightClover.png';
-import EightHeart from 'assets/cards/EightHeart.png';
-import EightPike from 'assets/cards/EightPike.png';
-import EightTile from 'assets/cards/EightTile.png';
-import FiveClover from 'assets/cards/FiveClover.png';
-import FiveHeart from 'assets/cards/FiveHeart.png';
-import FivePike from 'assets/cards/FivePike.png';
-import FiveTile from 'assets/cards/FiveTile.png';
-import FourClover from 'assets/cards/FourClover.png';
-import FourHeart from 'assets/cards/FourHeart.png';
-import FourPike from 'assets/cards/FourPike.png';
-import FourTile from 'assets/cards/FourTile.png';
-import Hades from 'assets/cards/Hades.png';
-import Heart from 'assets/cards/Heart.png';
-import HiddedCard from 'assets/cards/HiddedCard.png';
-import Hypnos from 'assets/cards/Hypnos.png';
-import JackClover from 'assets/cards/JackClover.png';
-import JackHeart from 'assets/cards/JackHeart.png';
-import JackPike from 'assets/cards/JackPike.png';
-import JackTile from 'assets/cards/JackTile.png';
-import JokerOuranos from 'assets/cards/JokerOuranos.png';
-import KingClover from 'assets/cards/KingClover.png';
-import KingHeart from 'assets/cards/KingHeart.png';
-import KingPike from 'assets/cards/KingPike.png';
-import KingTile from 'assets/cards/KingTile.png';
-import NineClover from 'assets/cards/NineClover.png';
-import NineHeart from 'assets/cards/NineHeart.png';
-import NinePike from 'assets/cards/NinePike.png';
-import NineTile from 'assets/cards/NineTile.png';
-import Pike from 'assets/cards/Pike.png';
-import QueenClover from 'assets/cards/QueenClover.png';
-import QueenHeart from 'assets/cards/QueenHeart.png';
-import QueenPike from 'assets/cards/QueenPike.png';
-import QueenTile from 'assets/cards/QueenTile.png';
-import SevenClover from 'assets/cards/SevenClover.png';
-import SevenHeart from 'assets/cards/SevenHeart.png';
-import SevenPike from 'assets/cards/SevenPike.png';
-import SevenTile from 'assets/cards/SevenTile.png';
-import SixClover from 'assets/cards/SixClover.png';
-import SixHeart from 'assets/cards/SixHeart.png';
-import SixPike from 'assets/cards/SixPike.png';
-import SixTile from 'assets/cards/SixTile.png';
-import TenClover from 'assets/cards/TenClover.png';
-import TenHeart from 'assets/cards/TenHeart.png';
-import TenPike from 'assets/cards/TenPike.png';
-import TenTile from 'assets/cards/TenTile.png';
-import ThreeClover from 'assets/cards/ThreeClover.png';
-import ThreeHeart from 'assets/cards/ThreeHeart.png';
-import ThreePike from 'assets/cards/ThreePike.png';
-import ThreeTile from 'assets/cards/ThreeTile.png';
-import Tile from 'assets/cards/Tile.png';
-import TwoClover from 'assets/cards/TwoClover.png';
-import TwoHeart from 'assets/cards/TwoHeart.png';
-import TwoPike from 'assets/cards/TwoPike.png';
-import TwoTile from 'assets/cards/TwoTile.png';
+import { ImageSourcePropType } from 'react-native';
 
-export {
-  Tile,
-  Pike,
-  Clover,
-  Heart,
-  JokerOuranos,
-  Artemis,
-  Hades,
-  Hypnos,
-  HiddedCard,
-  TwoClover,
-  ThreeClover,
-  FourClover,
-  FiveClover,
-  SixClover,
-  SevenClover,
-  EightClover,
-  NineClover,
-  TenClover,
-  JackClover,
-  QueenClover,
-  KingClover,
-  AceClover,
-  TwoTile,
-  ThreeTile,
-  FourTile,
-  FiveTile,
-  SixTile,
-  SevenTile,
-  EightTile,
-  NineTile,
-  TenTile,
-  JackTile,
-  QueenTile,
-  KingTile,
-  AceTile,
-  TwoHeart,
-  ThreeHeart,
-  FourHeart,
-  FiveHeart,
-  SixHeart,
-  SevenHeart,
-  EightHeart,
-  NineHeart,
-  TenHeart,
-  JackHeart,
-  QueenHeart,
-  KingHeart,
-  AceHeart,
-  TwoPike,
-  ThreePike,
-  FourPike,
-  FivePike,
-  SixPike,
-  SevenPike,
-  EightPike,
-  NinePike,
-  TenPike,
-  JackPike,
-  QueenPike,
-  KingPike,
-  AcePike,
+const NORMAL_SPADES_Q = require('./NORMAL_SPADES_Q.png');
+const ARTEMIS = require('./ARTEMIS.png');
+const HADES = require('./HADES.png');
+const HERMES = require('./HERMES.png');
+const HIDDEN_CARD = require('./HIDDEN_CARD.png');
+const HYPNOS = require('./HYPNOS.png');
+const JOKER = require('./JOKER.png');
+const NORMAL_CLUBS_2 = require('./NORMAL_CLUBS_2.png');
+const NORMAL_CLUBS_3 = require('./NORMAL_CLUBS_3.png');
+const NORMAL_CLUBS_4 = require('./NORMAL_CLUBS_4.png');
+const NORMAL_CLUBS_5 = require('./NORMAL_CLUBS_5.png');
+const NORMAL_CLUBS_6 = require('./NORMAL_CLUBS_6.png');
+const NORMAL_CLUBS_7 = require('./NORMAL_CLUBS_7.png');
+const NORMAL_CLUBS_8 = require('./NORMAL_CLUBS_8.png');
+const NORMAL_CLUBS_9 = require('./NORMAL_CLUBS_9.png');
+const NORMAL_CLUBS_10 = require('./NORMAL_CLUBS_10.png');
+const NORMAL_CLUBS_A = require('./NORMAL_CLUBS_A.png');
+const NORMAL_CLUBS_J = require('./NORMAL_CLUBS_J.png');
+const NORMAL_CLUBS_K = require('./NORMAL_CLUBS_K.png');
+const NORMAL_CLUBS_Q = require('./NORMAL_CLUBS_Q.png');
+const NORMAL_DIAMONDS_2 = require('./NORMAL_DIAMONDS_2.png');
+const NORMAL_DIAMONDS_3 = require('./NORMAL_DIAMONDS_3.png');
+const NORMAL_DIAMONDS_4 = require('./NORMAL_DIAMONDS_4.png');
+const NORMAL_DIAMONDS_5 = require('./NORMAL_DIAMONDS_5.png');
+const NORMAL_DIAMONDS_6 = require('./NORMAL_DIAMONDS_6.png');
+const NORMAL_DIAMONDS_7 = require('./NORMAL_DIAMONDS_7.png');
+const NORMAL_DIAMONDS_8 = require('./NORMAL_DIAMONDS_8.png');
+const NORMAL_DIAMONDS_9 = require('./NORMAL_DIAMONDS_9.png');
+const NORMAL_DIAMONDS_10 = require('./NORMAL_DIAMONDS_10.png');
+const NORMAL_DIAMONDS_A = require('./NORMAL_DIAMONDS_A.png');
+const NORMAL_DIAMONDS_J = require('./NORMAL_DIAMONDS_J.png');
+const NORMAL_DIAMONDS_K = require('./NORMAL_DIAMONDS_K.png');
+const NORMAL_DIAMONDS_Q = require('./NORMAL_DIAMONDS_Q.png');
+const NORMAL_HEARTS_2 = require('./NORMAL_HEARTS_2.png');
+const NORMAL_HEARTS_3 = require('./NORMAL_HEARTS_3.png');
+const NORMAL_HEARTS_4 = require('./NORMAL_HEARTS_4.png');
+const NORMAL_HEARTS_5 = require('./NORMAL_HEARTS_5.png');
+const NORMAL_HEARTS_6 = require('./NORMAL_HEARTS_6.png');
+const NORMAL_HEARTS_7 = require('./NORMAL_HEARTS_7.png');
+const NORMAL_HEARTS_8 = require('./NORMAL_HEARTS_8.png');
+const NORMAL_HEARTS_9 = require('./NORMAL_HEARTS_9.png');
+const NORMAL_HEARTS_10 = require('./NORMAL_HEARTS_10.png');
+const NORMAL_HEARTS_A = require('./NORMAL_HEARTS_A.png');
+const NORMAL_HEARTS_J = require('./NORMAL_HEARTS_J.png');
+const NORMAL_HEARTS_K = require('./NORMAL_HEARTS_K.png');
+const NORMAL_HEARTS_Q = require('./NORMAL_HEARTS_Q.png');
+const NORMAL_SPADES_2 = require('./NORMAL_SPADES_2.png');
+const NORMAL_SPADES_3 = require('./NORMAL_SPADES_3.png');
+const NORMAL_SPADES_4 = require('./NORMAL_SPADES_4.png');
+const NORMAL_SPADES_5 = require('./NORMAL_SPADES_5.png');
+const NORMAL_SPADES_6 = require('./NORMAL_SPADES_6.png');
+const NORMAL_SPADES_7 = require('./NORMAL_SPADES_7.png');
+const NORMAL_SPADES_8 = require('./NORMAL_SPADES_8.png');
+const NORMAL_SPADES_9 = require('./NORMAL_SPADES_9.png');
+const NORMAL_SPADES_10 = require('./NORMAL_SPADES_10.png');
+const NORMAL_SPADES_A = require('./NORMAL_SPADES_A.png');
+const NORMAL_SPADES_J = require('./NORMAL_SPADES_J.png');
+const NORMAL_SPADES_K = require('./NORMAL_SPADES_K.png');
+
+export type CardImages = {
+  [key: string]: ImageSourcePropType;
+};
+
+export const cardImages: CardImages = {
+  NORMAL_SPADES_Q,
+  ARTEMIS,
+  HADES,
+  HERMES,
+  HIDDEN_CARD,
+  HYPNOS,
+  JOKER,
+  NORMAL_CLUBS_2,
+  NORMAL_CLUBS_3,
+  NORMAL_CLUBS_4,
+  NORMAL_CLUBS_5,
+  NORMAL_CLUBS_6,
+  NORMAL_CLUBS_7,
+  NORMAL_CLUBS_8,
+  NORMAL_CLUBS_9,
+  NORMAL_CLUBS_10,
+  NORMAL_CLUBS_A,
+  NORMAL_CLUBS_J,
+  NORMAL_CLUBS_K,
+  NORMAL_CLUBS_Q,
+  NORMAL_DIAMONDS_2,
+  NORMAL_DIAMONDS_3,
+  NORMAL_DIAMONDS_4,
+  NORMAL_DIAMONDS_5,
+  NORMAL_DIAMONDS_6,
+  NORMAL_DIAMONDS_7,
+  NORMAL_DIAMONDS_8,
+  NORMAL_DIAMONDS_9,
+  NORMAL_DIAMONDS_10,
+  NORMAL_DIAMONDS_A,
+  NORMAL_DIAMONDS_J,
+  NORMAL_DIAMONDS_K,
+  NORMAL_DIAMONDS_Q,
+  NORMAL_HEARTS_2,
+  NORMAL_HEARTS_3,
+  NORMAL_HEARTS_4,
+  NORMAL_HEARTS_5,
+  NORMAL_HEARTS_6,
+  NORMAL_HEARTS_7,
+  NORMAL_HEARTS_8,
+  NORMAL_HEARTS_9,
+  NORMAL_HEARTS_10,
+  NORMAL_HEARTS_A,
+  NORMAL_HEARTS_J,
+  NORMAL_HEARTS_K,
+  NORMAL_HEARTS_Q,
+  NORMAL_SPADES_2,
+  NORMAL_SPADES_3,
+  NORMAL_SPADES_4,
+  NORMAL_SPADES_5,
+  NORMAL_SPADES_6,
+  NORMAL_SPADES_7,
+  NORMAL_SPADES_8,
+  NORMAL_SPADES_9,
+  NORMAL_SPADES_10,
+  NORMAL_SPADES_A,
+  NORMAL_SPADES_J,
+  NORMAL_SPADES_K,
 };
